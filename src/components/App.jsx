174d1f6d563b2b2cdac981fb677950e6f97ceb10
Volodya Kushch <volodyakushch@gmail.com>
2023-03-01 17:36:53 +0200

    com

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,41 @@
+import React from 'react';
+
+import user from 'user';
+import data from 'data';
+import friends from 'friends';
+import transactions from 'transactions';
+
+import Profile from '../components/Profile/Profile';
+
+import Statistic from '../components/Statistics/Statistic';
+
+import Friends from '../components/FriendList/Friends';
+
+import Transactions from '../components/Transactions/Transactions';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    ></div>
+    <section>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
+      <Statistic title stats={data} />
+      <Friends data={friends} />
+      <Transactions data={transactions} />
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      ></div>
+    </section>
   );
 };
